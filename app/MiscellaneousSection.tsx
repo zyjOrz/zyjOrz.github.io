a6b20b71306a831345cab01b0dc7da6d0f4654a0
zyjOrz <2124753995@qq.com>
2026-09-23 16:12:57 -0700

    iclr

--- app/MiscellaneousSection.tsx
+++ app/MiscellaneousSection.tsx
@@ -46,7 +46,7 @@ export default function MiscellaneousSection() {
 
   <div className="space-y-1 text-[0.98rem] leading-7 text-[#4c3e45]">
     <p className="m-0">
-      📄 Reviewer for  
+      📄 Reviewer for  {' '}
       <span className="font-semibold text-[#3a2b33]">
          ICLR 
       </span>.
